32d0f5545fe7c9dcb37a5ed3fafaf9020105d632
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-12-01 13:10:57 +0200

    feat: add fs/copy.js solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import { readdir, mkdir, copyFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const sourceFolder = join(__dirname, 'files');
+    const targetFolder = `${sourceFolder}_copy`;
+
+    try {
+        const files = await readdir(sourceFolder);
+
+        await mkdir(targetFolder);
+        files.forEach((file) => copyFile(`${sourceFolder}/${file}`,`${targetFolder}/${file}`));
+    }
+    catch (err) {
+        if (err.code === 'EEXIST' || err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        throw err;
+    }
 };
 
 copy();
