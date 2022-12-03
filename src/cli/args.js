cd9211d8903c6aa59469caeda6eeb19d0cf8fb87
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-12-03 11:29:23 +0200

    feat: add cli/args.js solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    let res = '';
+
+    process.argv.slice(2)
+        .forEach((item, idx, arr) => {
+            if (item.startsWith('--')) {
+                res += `${item.slice(2)} is ${arr[idx + 1]}, `;
+            }
+        });
+    res = res.slice(0, -2);
+    console.log(res);
 };
 
 parseArgs();
