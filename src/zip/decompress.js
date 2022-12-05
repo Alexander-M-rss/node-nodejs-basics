7202666a35836eafacb1260c7706f70622e3857b
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-12-05 11:56:44 +0200

    feat: add zip solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import {  pipeline } from 'stream/promises';
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const sourcePath = join(__dirname, "files", "archive.gz");
+    const destinationPath = join(__dirname, "files", "fileToCompress.txt");
+    const source = createReadStream(sourcePath);
+    const destination = createWriteStream(destinationPath);
+    const gunzip = createGunzip();
+
+    await pipeline(source, gunzip, destination);
 };
 
 await decompress();
